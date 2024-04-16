13b6ff14a7867c3ceff71ad3231bd69ea43b8855
Syeda Imaan Zahra <zahrasyedaimaan@gmail.com>
2024-04-15 20:37:35 -0700

    created a simple To Dos

---
+++ index.ts
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+
+while (condition) {
+  let addTask = await inquirer.prompt([
+    {
+      name: "toDos",
+      type: "input",
+      message: "What would you like to add to your TO-DOs?",
+    },
+    {
+      name: "addMore",
+      type: "confirm",
+      message: "Would you like to add more?",
+      default: "false",
+    },
+  ]
+);
+  todos.push(addTask.toDos),
+  condition=addTask.addMore
+  console.log(todos)
+
+}
